ff3f50b368a64b709409008ffce8d678613f32cc
Shivani <shivanitripurari07@gmail.com>
2025-10-14 09:25:47 +0530

    day3 updated

--- App.js
+++ App.js
@@ -1,50 +1,27 @@
         import React from "react";
         import {createRoot} from "react-dom/client"
 
-        //creating a h1 tag using React
-        //what are we passing to createElement ?
-        //1. element name / tag name
-        //2. {} -> pass tag's atribute in this object (attributes)
-        //3. what we want inside the tag/element (children)
-        const heading = React.createElement("h1", {id:"heading"}, "Hello World Using React");
 
-        console.log("heading ", heading);
-        //React.createElement is just a javascript object
 
-        //creating a root element 
+
+        //React.createElement -> creates a js object
+        // after rendering this to DOM, -> becomes html element
+        // const heading = React.createElement("h1", {id:"heading",className:"font-bold, text-red-300"}, "created h1 element");
+        const heading = <h1>Jsx heading🚀</h1>
+        console.log("heading 10 ", heading);
+        //this heading is jsx element not react element (because its not a funtion that returns jsx)
+
+        //functional components
+        const Hero = () => {
+            return(
+                <div>
+                    {heading}
+                    <h1>This is Hero section</h1>
+                    <p>hero...</p>
+                </div>
+            )
+        }
+
         const root = createRoot(document.getElementById("root"));
-
-        //adding the heading to root
-        // root.render(heading);
-        
-        //render function converts the js object into HTML element that Browser understands
-        
-        //creating a nested structure only using React 
-
-        /**
-         * <div id="parent">
-         *     <div id="node1">
-         *         <h1 id="heading1">I'm heading 1</h1>
-         *         <h2 id="heading2">I'm heading 2</h2>
-         *     </div>
-         *     <div id="node2">
-         *         <h1 id="heading3">I'm heading 3</h1>
-         *         <h2 id="heading4">I'm heading 4</h2>
-         *     </div>
-         * </div>
-         */
-
-        const parent = React.createElement("div", {id:"parent"},[
-            React.createElement("div", {id:"node1"}, [
-                React.createElement("h1", {id:"heading1"}, "I'm heading 1"),
-                React.createElement("h2", {id:"heading2"}, "I'm heading 2")
-            ]),
-            React.createElement("div", {id:"node2"}, [
-                React.createElement("h1", {id:"heading3"}, "I'm heading 3"),
-                React.createElement("h2", {id:"heading4"}, "I'm heading 4"),
-            ])
-        ] );
-        console.log("this is parent ", parent);
-
-        root.render(parent);
-        
+        console.log("root 12 ", root);
+        root.render(<Hero/>);
